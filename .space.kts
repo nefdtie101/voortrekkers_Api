c7a0c46462f2691dc1a29e4bc3e991ff04da6ea2
Johann Nefdt <johann@nefdtco.co.za>
2022-12-26 19:16:19 +0000

    New file .space.kts

---
+++ .space.kts
@@ -0,0 +1,26 @@
+/**
+* JetBrains Space Automation
+* This Kotlin-script file lets you automate build activities
+* For more info, see https://www.jetbrains.com/help/space/automation.html
+*/
+
+job("Build and push Docker") {
+    host("Build artifacts and a Docker image") {
+      
+
+        dockerBuildPush {
+            // Docker context, by default, project root
+            // path to Dockerfile relative to project root
+            // if 'file' is not specified, Docker will look for it in 'context'/Dockerfile
+             file = "Dockerfile"
+         
+
+            val spaceRepo = "nefdtco.registry.jetbrains.space/p/voortrekkers/docker/voortrekker_api"
+            // image tags for 'docker push'
+            tags {
+                +"$spaceRepo:1.0.${"$"}JB_SPACE_EXECUTION_NUMBER"
+                +"$spaceRepo:latest"
+            }
+        }
+    }
+}
